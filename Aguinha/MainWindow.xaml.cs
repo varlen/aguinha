53590d318d058d4d4f097601b781a8406c89ea47
Varlen <varlenneto@poli.ufrj.br>
2021-02-20 17:06:05 -0300

    Bind list of glasses to glass dropdown

--- Aguinha/MainWindow.xaml.cs
+++ Aguinha/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Aguinha.Models;
+
 namespace Aguinha
 {
     /// <summary>
@@ -20,10 +22,30 @@ namespace Aguinha
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private List<Glass> Glasses = new List<Glass>{
+            new Glass { 
+                Name = "Copo Americano", 
+                CapacityMililiters = 200, 
+                Image = new Uri("/Images/copo americano.jpg", UriKind.Relative)
+            },
+            new Glass
+            {
+                Name = "Copão",
+                CapacityMililiters = 350,
+                Image = new Uri("/Images/pint.jpg", UriKind.Relative)
+            }
+        };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.GlassDropdown.ItemsSource = this.Glasses;
+            this.GlassDropdown.SelectedIndex = 0;
         }
+
+
 
         private void DrinkWaterButton_Click(object sender, RoutedEventArgs e)
         {
--- Aguinha/Models/Glass.cs
+++ Aguinha/Models/Glass.cs
@@ -9,5 +9,10 @@ namespace Aguinha.Models
         public string Name { get; set; }
         public int CapacityMililiters { get; set; }
         public Uri Image { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {CapacityMililiters} ml";
+        }
     }
 }
